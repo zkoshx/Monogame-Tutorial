5e62305718fe1f391eef7585de12b12cf48f1a74
Neo Akosile <neoakosile7@gmail.com>
2021-12-01 14:15:11 +0000

    Started Tutorial - Created Target - Created Score Counter - Created a bunch of stuff

--- Game1.cs
+++ Game1.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+
 
 namespace Learning_Monogame
 {
@@ -8,6 +10,18 @@ namespace Learning_Monogame
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+
+        Texture2D targetSprite;
+        Texture2D crosshairSprite;
+        Texture2D bgSprite;
+        SpriteFont gameFont;
+
+        Vector2 targetPos = new Vector2(300, 300);
+        const int rad = 45;
+
+        MouseState mState;
+        bool mReleased = true;
+        int score = 0;
 
         public Game1()
         {
@@ -26,6 +40,10 @@ namespace Learning_Monogame
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+            targetSprite = Content.Load<Texture2D>("target");
+            crosshairSprite = Content.Load<Texture2D>("crosshairs");
+            bgSprite = Content.Load<Texture2D>("sky");
+            gameFont = Content.Load<SpriteFont>("galleryFont");
 
             // TODO: use this.Content to load your game content here
         }
@@ -35,7 +53,23 @@ namespace Learning_Monogame
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            mState = Mouse.GetState();
+
+            if (mState.LeftButton == ButtonState.Pressed && mReleased == true)
+            {
+                float mouseTargetDist = Vector2.Distance(targetPos, mState.Position.ToVector2());
+                if (mouseTargetDist < rad)
+                {
+                    score++;
+                    Random rnd = new Random();
+                }
+                mReleased = false;
+            }
+
+            if (mState.LeftButton == ButtonState.Released)
+            {
+                mReleased = true;
+            }
 
             base.Update(gameTime);
         }
@@ -43,8 +77,12 @@ namespace Learning_Monogame
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-
-            // TODO: Add your drawing code here
+            
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(bgSprite, new Vector2(0, 0), Color.White);
+            _spriteBatch.DrawString(gameFont, score.ToString(), new Vector2(100,0), Color.Blue);
+            _spriteBatch.Draw(targetSprite, new Vector2(targetPos.X - rad, targetPos.Y - rad), Color.White);
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
